6de4abaf29856997e3791c552a5b12b3c96b1182
minhduc5a15 <minhduc5a15@gmail.com>
2025-01-12 04:09:50 +0700

    up

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,16 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import { db, ref, get, set } from '../src/utils/firebase';
 import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 app.get('/api/visit', async (req, res) => {
     try {
