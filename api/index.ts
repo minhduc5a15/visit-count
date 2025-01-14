8c94406ab7121bb819b8c83c0c5f7a4fad7357ac
minhduc5a15 <minhduc5a15@gmail.com>
2025-01-15 04:09:27 +0700

    use mongodb

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,21 @@
 import express, { urlencoded } from 'express';
-import { db, ref, get, set } from '../src/utils/firebase';
 import cors from 'cors';
+import crypto from 'crypto';
+import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
 import { formatNumber } from '../src/utils/format-number';
 import { axiosInstance } from '../src/utils/axios';
-import crypto from 'crypto';
+import { getCollecion } from '../src/lib/mongodb';
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests, please try again later.',
+}) 
+
+app.use(limiter);
 app.use(
     cors({
         origin: '*',
@@ -16,41 +25,36 @@ app.use(
 );
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('combined'));
 
 app.get('/', (_, res) => {
-    res.redirect('https://github.com/minhduc5a15/visit-count');
+    return res.redirect('https://github.com/minhduc5a15/visit-count');
 });
-
 
 app.get('/api/visit', async (req, res) => {
-    try {
-        const visitRef = ref(db, 'visit_count');
-        const snapshot = await get(visitRef);
-        let count = snapshot.exists() ? snapshot.val() : 0;
+    const visitCountColl = await getCollecion('visit-count');
+    await visitCountColl.updateOne({ type: 'visit_count' }, { $inc: { visit_count: 1 } }, { upsert: true });
+    
+    const visitorIpsColl = await getCollecion('visitor-ips');
+    const ip = (
+        (typeof req.headers['x-forwarded-for'] === 'string'
+            ? req.headers['x-forwarded-for']
+            : req.connection.remoteAddress) || 'unknown'
+    ).replace(/\./g, '_');
+    const referer = req.headers['referer'] || req.headers['origin'] || 'unknown';
 
-        count += 1;
-        await set(visitRef, count);
+    const timestamp = Date.now();
+    const uniqueKey = `${ip}-${timestamp}-${crypto.randomBytes(4).toString('hex')}`;
 
-        const ip = ((typeof req.headers['x-forwarded-for'] === 'string' ? req.headers['x-forwarded-for'] : req.connection.remoteAddress) || 'unknown').replace(/\./g, '_');
-        const referer = req.headers['referer'] || req.headers['origin'] || 'unknown';
+    const visitCountDoc = await visitCountColl.findOne({ type: 'visit_count' });
+    await visitorIpsColl.insertOne({ key: uniqueKey, ip, timestamp: new Date(timestamp).toISOString(), referer , current_visit: visitCountDoc ? visitCountDoc['visit_count'] : 0 });
 
-        const timestamp = Date.now();
-        const uniqueKey = `${ip}-${timestamp}-${crypto.randomBytes(4).toString('hex')}`;
+    const response = await axiosInstance.get('/api/code/fba61808');
 
-        const ipRef = ref(db, `visitor_ips/${uniqueKey}`);
-        await set(ipRef, { ip, timestamp: new Date(timestamp).toISOString(), referer });
-
-        const response = await axiosInstance.get('/api/code/fba61808');
-
-        res.setHeader('Content-Type', 'image/svg+xml');
-        res.setHeader('X-Content-Type-Options', 'nosniff');
-        res.send(response.data.content.replace('{count}', formatNumber(count)));
-    } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-        res.status(500).send('Error: ' + errorMessage);
-    }
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    return res.send(response.data.content.replace('{count}', formatNumber(visitCountDoc ? visitCountDoc['visit_count'] : 0)));
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
